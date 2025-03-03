7eccd85fa2918972706580ec2a7b95db0ff83d19
Leon Shan <144356027+Holycious@users.noreply.github.com>
2025-03-03 11:47:42 +0700

    Jobsheet 4 | Praktikum 1 | $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,9 +11,11 @@ class UserController extends Controller
     public function index()
     {
         $data = [
-            'nama' => 'Pelanggan Pertama'
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3'
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
         
         $user = UserModel::all();
         return view('user',['data' => $user]);
