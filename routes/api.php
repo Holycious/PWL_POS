be1d455c6c10de147f0dd58894c4bdeec5503c21
Leon Shan <144356027+Holycious@users.noreply.github.com>
2025-04-28 20:01:04 +0700

    Jobsheet 10 | Tugas

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,21 @@ Route::post('levels', [App\Http\Controllers\Api\LevelController::class, 'store']
 Route::get('levels/{level}', [App\Http\Controllers\Api\LevelController::class, 'show']);
 Route::put('levels/{level}', [App\Http\Controllers\Api\LevelController::class, 'update']);
 Route::delete('levels/{level}', [App\Http\Controllers\Api\LevelController::class, 'destroy']);
+
+Route::get('users', [App\Http\Controllers\Api\UserController::class, 'index']);
+Route::post('users', [App\Http\Controllers\Api\UserController::class, 'store']);
+Route::get('users/{user}', [App\Http\Controllers\Api\UserController::class, 'show']);
+Route::put('users/{user}', [App\Http\Controllers\Api\UserController::class, 'update']);
+Route::delete('users/{user}', [App\Http\Controllers\Api\UserController::class, 'destroy']);
+
+Route::get('kategori', [App\Http\Controllers\Api\KategoriController::class, 'index']);
+Route::post('kategori', [App\Http\Controllers\Api\KategoriController::class, 'store']);
+Route::get('kategori/{kategori}', [App\Http\Controllers\Api\KategoriController::class, 'show']);
+Route::put('kategori/{kategori}', [App\Http\Controllers\Api\KategoriController::class, 'update']);
+Route::delete('kategori/{kategori}', [App\Http\Controllers\Api\KategoriController::class, 'destroy']);
+
+Route::get('barang', [App\Http\Controllers\Api\BarangController::class, 'index']);
+Route::post('barang', [App\Http\Controllers\Api\BarangController::class, 'store']);
+Route::get('barang/{barang}', [App\Http\Controllers\Api\BarangController::class, 'show']);
+Route::put('barang/{barang}', [App\Http\Controllers\Api\BarangController::class, 'update']);
+Route::delete('barang/{barang}', [App\Http\Controllers\Api\BarangController::class, 'destroy']);
